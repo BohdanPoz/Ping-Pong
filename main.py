6dd4ca26cceb12d6889611731a1cf8c96eba02c9
BohdanPoz <bohdanpoznanskyi08@gmail.com>
2023-04-14 16:35:33 +0300

    second commit

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import menu
 
 pygame.init()
 
-width, height = 900, 700
+WIDTH, HEIGHT = 900, 700
 FPS = 40
 
-window = pygame.display.set_mode((width, height))
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ping-Pong")
 
 clock = pygame.time.Clock()
@@ -20,12 +20,18 @@ def single_b():
     global player_regim
     play_b()
     player_regim = False
+    platform0.center = start_coords[0]
+    platform1.center = start_coords[1]
+    ball.center = start_coords[2]
 
 def double_b():
     global player_regim
     global menu_regim
     play_b()
     player_regim = True
+    platform0.center = start_coords[0]
+    platform1.center = start_coords[1]
+    ball.center = start_coords[2]
 
 def restart_b():
     global menu_regim
@@ -38,8 +44,34 @@ def main_menu_b():
     global menu_regim
     menu_regim = 0
 
-platform0 = pygame.Rect(385, 650, 130, 30)
-platform1 = pygame.Rect(385, 20, 130, 30)
+class Platform(pygame.Rect):
+    def __init__(self, x, y, width, height, step, color, control='player0'):
+        super().__init__(x, y, width, height)
+
+        self.COLOR = color
+        self.CONTROL = control
+        self.STEP = step
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.COLOR, self)
+
+    def move(self, ball):
+        if 'player' in self.CONTROL:
+            if self.CONTROL[-1] == '0':
+                control_keys = (pygame.K_RIGHT, pygame.K_LEFT)
+            else:
+                control_keys = (pygame.K_d, pygame.K_a)
+            keys = pygame.key.get_pressed()
+
+            if keys[control_keys[0]] and not self.right >= WIDTH:
+                self.x += self.STEP
+            if keys[control_keys[1]] and not self.left <= 0:
+                self.x -= self.STEP
+        elif self.CONTROL == 'computor':
+            self.centerx = ball.centerx
+
+platform0 = Platform(WIDTH//2-130//2, HEIGHT-40, 130, 30, 5, (200, 0, 0))
+platform1 = Platform(WIDTH//2-130//2, 20, 130, 30, 5, (0, 0, 200), 'computor')
 
 ball = pygame.Rect(485, 285, 30, 30)
 
@@ -72,6 +104,11 @@ def run():
     player_regim = False
 
     while game:
+        if not player_regim and platform1.CONTROL != 'computor':
+            platform1.CONTROL = 'computor'
+        elif player_regim and platform1.CONTROL != 'player1':
+            platform1.CONTROL = 'player1'
+
         if menu_regim == 0:
             main_menu.show()
 
@@ -87,8 +124,8 @@ def run():
         elif menu_regim == 1:
             window.fill((50, 50, 50))
 
-            pygame.draw.rect(window, (200, 0, 0), platform0)
-            pygame.draw.rect(window, (0, 0, 200), platform1)
+            platform0.draw(window)
+            platform1.draw(window)
             pygame.draw.circle(window, (0, 200, 0), ball.center, 15)
 
             for event in pygame.event.get():
@@ -97,34 +134,21 @@ def run():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         menu_regim = 2
-                
 
-            keys = pygame.key.get_pressed()
-
-            if keys[pygame.K_RIGHT] and not platform0.right >= width:
-                platform0.x += 5
-            if keys[pygame.K_LEFT] and not platform0.left <= 0:
-                platform0.x -= 5
-
-            if player_regim:
-                if keys[pygame.K_d] and not platform1.right >= width:
-                    platform1.x += 5
-                if keys[pygame.K_a] and not platform1.left <= 0:
-                    platform1.x -= 5
-            else:
-                platform1.centerx = ball.centerx
+            platform0.move(ball)
+            platform1.move(ball)
             
             ball.y += step_bally
             ball.x += step_ballx
 
             if ball.colliderect(platform0) or ball.colliderect(platform1):
                 step_bally = -step_bally
-            if ball.left <= 0 or ball.right >= width:
+            if ball.left <= 0 or ball.right >= WIDTH:
                 step_ballx = -step_ballx
 
             if ball.top <= 0:
                 menu_regim = 3
-            elif ball.bottom >= height:
+            elif ball.bottom >= HEIGHT:
                 menu_regim = 4
 
         elif menu_regim == 2:
